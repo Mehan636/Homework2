282a4f86246b15a1706e3af23fb7cbef97c6d3d2
Mehan636 <167680307+Mehan636@users.noreply.github.com>
2024-07-17 10:31:02 +0300

    homework2.py

--- homework2.py
+++ homework2.py
@@ -2,7 +2,7 @@ number_of_completed_homework_assignments = 12
 Number_of_hours_spent = 1.5
 Course_name = 'Python'
 Time_for_one_homework_assignment = (Number_of_hours_spent / number_of_completed_homework_assignments)
-print('Курс: '+Course_name+', всего задач:'+str(number_of_completed_homework_assignments)+ ', затрачено часов: ' +str(Number_of_hours_spent)+', среднее время выполнения', Time_for_one_homework_assignment, 'часа.' )
+print('Курс: '+Course_name+', всего задач:'+str(number_of_completed_homework_assignments)+ ', затрачено часов: ' +str(Number_of_hours_spent)+', среднее время выполнения '+str(Time_for_one_homework_assignment)+ ' часа.' )
 
 
 
