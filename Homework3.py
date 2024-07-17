7538b74aa3f5d61ef408f57ae9a9416fe69188c1
Mehan636 <167680307+Mehan636@users.noreply.github.com>
2024-07-17 11:42:01 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,14 @@
+Name = 'Pavel'
+print(Name)
+Age = 33
+print(Age)
+New_Age = Age+1
+print(New_Age)
+Is_Student = True
+print('Is Student:', Is_Student)
+
+
+print(type(Name))
+print(type(Age))
+print(type(New_Age))
+print(type(Is_Student))
