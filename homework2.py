c7bf82f1bca3388a7e47a9ac05e6d45360975ad7
Mehan636 <167680307+Mehan636@users.noreply.github.com>
2024-07-17 10:25:26 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,10 @@
+number_of_completed_homework_assignments = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_homework_assignment = (Number_of_hours_spent / number_of_completed_homework_assignments)
+print('Курс: '+Course_name+', всего задач:'+str(number_of_completed_homework_assignments)+ ', затрачено часов: ' +str(Number_of_hours_spent)+', среднее время выполнения', Time_for_one_homework_assignment, 'часа.' )
+
+
+
+#образец итога "Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа."
+#мой итог       Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
